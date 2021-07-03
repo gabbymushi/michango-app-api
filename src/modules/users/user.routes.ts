d63a8f0dd86156ffcf23cb95d104397fcb4f1f6f
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-07-03 18:24:14 +0300

    code cleaning and refactoring

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -10,8 +10,6 @@ router.post('/', UserController.createUser);
 router.post('/validate-pin', isAuthenticated, UserController.validatePaymentPin);
 router.patch('/:id', UserController.updateUser);
 router.delete('/:id', UserController.deleteUser);
-router.post('/verify-nida', UserController.verifyNIDA);
-router.post('/answer-nida', UserController.sendNIDAQuestion);
 router.post('/change-payment-pin', isAuthenticated, UserController.changePaymentPIN);
 
 export const UserRoutes = router;
