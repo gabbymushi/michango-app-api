d8f8ee4a37181c805e7474c97d6ed4d0ce058b66
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-08-01 23:19:18 +0300

    add contributors route to middleware

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -4,6 +4,7 @@ import { OtpRoutes } from './otp/otp.routes';
 import { AuthRoutes } from './auth/auth.routes';
 import { ConfigRoutes } from './configurations/config.routes';
 import { EventRoutes } from './events/event.routes';
+import { ContributorRoutes } from './contributors/contributor.routes';
 
 export const initializeRoutes = (app: express.Application) => {
     app.use('/api/v1/users/', UserRoutes);
@@ -11,4 +12,5 @@ export const initializeRoutes = (app: express.Application) => {
     app.use('/api/v1/auth/', AuthRoutes);
     app.use('/api/v1/configs/', ConfigRoutes);
     app.use('/api/v1/events/', EventRoutes);
+    app.use('/api/v1/contributors/', ContributorRoutes);
 }
