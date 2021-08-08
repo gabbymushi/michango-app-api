64e2b62eef5f645ee18cf277cf1bfcc3429ecb10
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-08-08 21:54:52 +0300

    added route to get events by owner

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -2,10 +2,12 @@ import express from 'express';
 const router = express.Router();
 import * as UserController from './user.controller';
 import { isAuthenticated } from '../auth/auth.controller';
+import { getEventsByOwnerId } from '../events/event.controller';
 
 router.get('/phone-number', UserController.getUserByPhoneNumber);
 router.get('/', UserController.getUsers);
 router.get('/:id', UserController.getUser);
+router.get('/:id/events',getEventsByOwnerId);
 router.post('/', UserController.createUser);
 router.patch('/:id', UserController.updateUser);
 router.delete('/:id', UserController.deleteUser);
